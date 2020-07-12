e009ef80b1f79a6a5bdabd7ee576bfb78f987cc6
Michael Figueroa <michaelfigueroa24@gmail.com>
2020-07-11 19:19:21 -0500

    cleanup

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ log = open("log.txt", "w")
 
 url = 'https://www.uhd.edu/academics/sciences/Pages/sciences-scholarships.aspx'
 
+print("Searching: " + str(url) + " for scholarships..." + "\n")
+
 response = requests.get(url, timeout=20)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
 
+
 section = soup.find('ul', class_='f_standard')
 
 for item in section.find_all('li'):
-    item = item.a.text 
+    item = item.a.text
+    log.write(str(item) + "\n")
+    log.write("\n")
     print(item + "\n")
